1e25bb9bf5fe9b5a4ca529cc714fefae35ed47a5
Mukesh Chaudhary <mukesh.chaudhary@navyuginfo.com>
2018-06-04 00:24:39 +0530

    Todos and items controllers

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  # Associatin test
+
+  # Association test
   # ensure an item record belongs to a single todo record
   it { should belong_to(:todo) }
+
   # Validation test
   # ensure column name is present before saving
   it { should validate_presence_of(:name) }
